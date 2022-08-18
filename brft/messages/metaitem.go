617f69cdd3ae40894acf34457992562df44dc567
Michael Hegel <ge82xiz@mytum.de>
2022-08-18 21:37:36 +0200

    added meta* packet (de-)serialization

--- brft/messages/metaitem.go
+++ brft/messages/metaitem.go
@@ -32,8 +32,6 @@ func NewMetaItem(
 	}, nil
 }
 
-// TODO: Move checks into the New* function in order to later be able to use
-// this function to test the server with invalid input
 func (m *MetaItem) Marshal() ([]byte, error) {
 	fileName := []byte(m.FileName)
 	if len(fileName) > 255 {
@@ -41,23 +39,29 @@ func (m *MetaItem) Marshal() ([]byte, error) {
 	} else if len(fileName) == 0 {
 		return nil, errors.New("filename empty")
 	}
+	outLen := 1 + len(m.FileName)
 
-	fileLen := 1 + len(m.FileName)
-	b := cryptobyte.NewFixedBuilder(make([]byte, 0, fileLen))
+	// check whether we should optionally add the filesize and checksum
+	extended := false
+	if m.FileSize != nil && len(m.Checksum) != 0 {
+		extended = true
+		outLen += 8 + common.ChecksumSize
+	}
+
+	// make sure either both or none of the optional parameters are set (!XOR)
+	if (m.FileSize == nil) != (len(m.Checksum) == 0) {
+		return nil, errors.New("file size OR checksum empty, but not both")
+	}
 
 	// write the filename
+	b := cryptobyte.NewFixedBuilder(make([]byte, 0, outLen))
 	b.AddUint8LengthPrefixed(func(b *cryptobyte.Builder) {
 		b.AddBytes(fileName)
 	})
 
-	// if the file size and checksum are not set we can return early
-	if m.FileSize == nil && len(m.Checksum) != 0 {
+	// if no optional fields are set, we can return early
+	if !extended {
 		return b.Bytes()
-	}
-
-	// optionally write the filesize and checksum
-	if m.FileSize == nil || len(m.Checksum) != 0 {
-		return nil, errors.New("file size OR checksum empty, but not both")
 	}
 
 	// Make sure the checksum length is valid
@@ -71,9 +75,13 @@ func (m *MetaItem) Marshal() ([]byte, error) {
 	return b.Bytes()
 }
 
-func (m *MetaItem) Unmarshal(data []byte) error {
+func (m *MetaItem) Unmarshal(data []byte, extended bool) error {
 	s := cryptobyte.String(data)
 
+	return m.UnmarshalWithString(&s, extended)
+}
+
+func (m *MetaItem) UnmarshalWithString(s *cryptobyte.String, extended bool) error {
 	// read the filename
 	var fileName cryptobyte.String
 	if !s.ReadUint8LengthPrefixed(&fileName) {
@@ -81,18 +89,22 @@ func (m *MetaItem) Unmarshal(data []byte) error {
 	}
 	m.FileName = string(fileName)
 
-	// TODO: It's not really ideal, but we do ot really have an indicator whether
+	// TODO: It's not really ideal, but we do not really have an indicator whether
 	// 		the filesize and checksum will be present
-	// try to read the file size, if it does not work we
-	var size uint64
-	if !ReadUint64(&s, &size) {
+	if !extended {
 		return nil
+	}
+
+	// try to read the file size
+	var size uint64
+	if !ReadUint64(s, &size) {
+		return errors.New("extended item, but unable to read file size")
 	}
 	m.FileSize = &size
 
 	checksum := make([]byte, 0, common.ChecksumSize)
 	if !s.ReadBytes(&checksum, common.ChecksumSize) {
-		return ErrReadFailed
+		return errors.New("extended item, but unable to read checksum")
 	}
 	m.Checksum = checksum
 
--- brft/messages/metaresp.go
+++ brft/messages/metaresp.go
@@ -1,1 +1,79 @@
 package messages
+
+import (
+	"errors"
+	"fmt"
+
+	"golang.org/x/crypto/cryptobyte"
+)
+
+type MetaResp struct {
+	// Items are the items for all the requested files
+	Items []MetaItem
+}
+
+// NewMetaResp creates a new MetaResp. The number of items will be deeduced from the items. NOTE that the maximum number
+// of items in one resp is limited to 255 and that there might also be responses with 0 items
+func NewMetaResp(items []MetaItem) (*MetaResp, error) {
+	// TODO: we could also do the splitting of MetaResponses in here and return []*MetaResp
+	numItems := len(items)
+	if numItems > 255 {
+		return nil, errors.New("to many items")
+	}
+
+	return &MetaResp{
+		Items: items,
+	}, nil
+}
+
+func (m *MetaResp) Marshal() ([]byte, error) {
+
+	// again, make sure that there are not too many items
+	numItems := len(m.Items)
+	if numItems > 255 {
+		return nil, errors.New("to many items")
+	}
+
+	bytes := make([]byte, 0, len(m.Items)*40) // some guess about the average item size
+	for _, item := range m.Items {
+		b, err := item.Marshal()
+		if err != nil {
+			return nil, fmt.Errorf("unable to marshal item: %s", err)
+		}
+
+		bytes = append(bytes, b...)
+	}
+
+	outLen := 1 + len(bytes)
+	b := cryptobyte.NewFixedBuilder(make([]byte, 0, outLen))
+
+	b.AddUint8(uint8(numItems))
+	b.AddBytes(bytes)
+
+	return b.Bytes()
+}
+
+func (m *MetaResp) Unmarshal(data []byte, extended bool) error {
+	s := cryptobyte.String(data)
+
+	var numItems uint8
+	if !s.ReadUint8(&numItems) {
+		return ErrReadFailed
+	}
+
+	// NOTE: This is designed badly, since we cannot infer here how long an item
+	// will be
+	items := make([]MetaItem, 0, numItems)
+	for i := 0; i < int(numItems); i++ {
+		item := &MetaItem{}
+		err := item.UnmarshalWithString(&s, extended)
+		if err != nil {
+			return fmt.Errorf("unabel to unmarshal item: %s", err)
+		}
+
+		items = append(items, *item)
+	}
+
+	m.Items = items
+	return nil
+}
