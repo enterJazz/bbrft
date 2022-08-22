9b5649df220da1b3ce091361db560870db093989
Wlad Meixner <hi@iamwlad.com>
2022-08-22 15:39:55 +0200

    fix: update log interface

--- btp/log.go
+++ btp/log.go
@@ -1,16 +1,19 @@
 package btp
 
-import "go.uber.org/zap"
+import (
+	"gitlab.lrz.de/bbrft/btp/messages"
+	"go.uber.org/zap"
+)
 
 const (
 	FBTPHeaderProtocolVersion = "version"
 	FBTPHeaderMessageType     = "message_type"
 )
 
-func FHeaderProtocolVersion(v ProtocolVersion) zap.Field {
+func FHeaderProtocolVersion(v messages.ProtocolVersion) zap.Field {
 	return zap.Uint8(FBTPHeaderProtocolVersion, uint8(v))
 }
 
-func FHeaderMessageType(t MessageType) zap.Field {
+func FHeaderMessageType(t messages.MessageType) zap.Field {
 	return zap.Uint8(FBTPHeaderMessageType, uint8(t))
 }
