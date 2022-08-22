dc02420ddde77415c638191da47c9c0a66c31b2a
Robert Schambach <robert.schambach@tum.de>
2022-08-22 17:58:59 +0200

    --wip-- [skip ci]

--- brft/compression/gzip.go
+++ brft/compression/gzip.go
@@ -1,6 +1,10 @@
 package compression
 
-import "go.uber.org/zap"
+import (
+	"bytes"
+	"compress/gzip"
+	"go.uber.org/zap"
+)
 
 type GzipCompressor struct {
 	l *zap.Logger
@@ -15,10 +19,31 @@ func NewGzipCompressor(
 }
 
 func (c *GzipCompressor) Compress(chunk []byte) ([]byte, error) {
-	// TODO: @robert implement me
-	return nil, nil
+	var buf bytes.Buffer
+	zw := gzip.NewWriter(&buf)
+	_, err := zw.Write(chunk)
+	if err != nil {
+		c.l.Error(err.Error())
+		return nil, err
+	}
+	err = zw.Close()
+	if err != nil {
+		c.l.Error(err.Error())
+		return nil, err
+	}
+	return buf.Bytes(), err
 }
 func (c *GzipCompressor) Decompress(chunk []byte) ([]byte, error) {
-	// TODO: @robert implement me
-	return nil, nil
+	var buf bytes.Buffer
+	zr, err := gzip.NewReader(&buf)
+	if err != nil {
+		c.l.Error(err.Error())
+		return nil, err
+	}
+	_, err = zr.Read(chunk)
+	if err != nil {
+		c.l.Error(err.Error())
+		return nil, err
+	}
+	return buf.Bytes(), err
 }
