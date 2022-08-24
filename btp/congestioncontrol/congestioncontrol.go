04b0099866edb00821b731405d819bb63cb76707
Robert Schambach <robert.schambach@tum.de>
2022-08-24 09:59:32 +0200

    wip: add basic elastic tcp cca interface functions

--- btp/congestioncontrol/congestioncontrol.go
+++ btp/congestioncontrol/congestioncontrol.go
@@ -1,9 +1,24 @@
 package congestioncontrol
 
+import "go.uber.org/zap"
+
 type CongestionControlAlgorithm interface {
+	Init(*zap.Logger, int) CongestionControlAlgorithm
+
 	SentMessages(int)
 	ReceivedAcks(int)
 	NumFreeSend() int
 
+	CongAvoid()
+	UpdateRTT(int)
+	HandleEvent(CaEvent)
+
 	Name() string
 }
+
+type CaEvent int
+
+const (
+	Loss CaEvent = iota
+	Duplicate
+)
