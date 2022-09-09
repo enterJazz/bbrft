9b74d1c3b74febd1628ffad6d0b4623ce050e2d0
Wlad Meixner <hi@iamwlad.com>
2022-09-09 17:57:19 +0200

    fix: don't preamtively close connection

--- brft/stream.go
+++ brft/stream.go
@@ -128,7 +128,7 @@ func (c *Conn) CloseStream(
 	// close BRFT client after all streams finished
 	if allFinished {
 		c.conn.ResetReadTimeout()
-		c.Close()
+		// IDEA: close connections when no more open streams
 	}
 }
 
